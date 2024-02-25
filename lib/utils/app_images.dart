0e157f69e7905855e0ea62a75e987cd128d02a76
swaponahmmed59 <swaponahmmed59@gmail.com>
2024-02-25 10:23:19 +0600

    SplashScreen complate

---
+++ lib/utils/app_images.dart
@@ -0,0 +1,5 @@
+class AppImages{
+  static const String basePath = "assets/images";
+  static const String appLogo = "$basePath/appLogo.png";
+  static const String splashBg = "$basePath/splashBg.png";
+}
