5ba4ed8d58f5a5ea662f4c528b27def469cf2cbb
swaponahmmed59 <swaponahmmed59@gmail.com>
2024-02-25 11:24:59 +0600

    SplashScreen complate

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+
+class AppColors{
+
+  static Color primaryColor = const Color(0xFFFA1131);
+  static Color backgroundColor = const Color(0xFF111111);
+  static Color borderColor = const Color(0xFFFB415A);
+  static Color greyColor = const Color(0xFF8C8C8C);
+  static Color fieldColor = const Color(0xFFFFE7EA).withOpacity(0.31);
+
+
+
+
+}
