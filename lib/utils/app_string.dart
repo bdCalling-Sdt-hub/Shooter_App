dcbd3590bfaefe2433be18b129a7aa5289bfe1d5
swaponahmmed59 <swaponahmmed59@gmail.com>
2024-02-25 12:28:38 +0600

    OnboardingsScreen Complated

--- lib/utils/app_string.dart
+++ lib/utils/app_string.dart
@@ -1,9 +1,9 @@
 class AppString {
   //================> Onboardings Screen <====================
-  static String smallBore = "Small-Bore Shooting";
+  static String smallBore = "Small-Bore Shooting South Africa";
   static String southAfrica = "South Africa";
   static String exciting =
-      "Exciting competitions with small guns, aiming at targets for precision and safety in South Africa's vibrant shooting scene.";
+      "Exciting competitions with small guns,\n aiming at targets for precision and \nsafety in South Africa's vibrant shooting \nscene.";
   static String logIn = "Log In";
   static String signUp = "Sign Up";
 }
--- lib/views/screens/onboardingsScreen/onboardings_screen.dart
+++ lib/views/screens/onboardingsScreen/onboardings_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
+import 'package:shooter_app/utils/app_colors.dart';
+import 'package:shooter_app/utils/app_string.dart';
+import 'package:shooter_app/utils/dimentions.dart';
+import 'package:shooter_app/views/widgets/custom_button.dart';
+import 'package:shooter_app/views/widgets/custom_text.dart';
 import '../../../utils/app_images.dart';
 
 class OnboardingsScreen extends StatelessWidget {
@@ -18,11 +22,41 @@ class OnboardingsScreen extends StatelessWidget {
               image: AssetImage(AppImages.onboardingsBg), fit: BoxFit.cover),
         ),
         child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 16.w),
+          padding: const EdgeInsets.symmetric(
+              horizontal: Dimensions.paddingSizeDefault),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-
+              const Spacer(),
+              CustomText(
+                text: AppString.smallBore,
+                maxline: 2,
+                fontWeight: FontWeight.w400,
+                fontsize: 32.h,
+                fontName: 'Aldrich',
+              ),
+              SizedBox(height: 16.h),
+              CustomText(
+                text: AppString.exciting,
+                fontWeight: FontWeight.w400,
+                fontsize: 16.h,
+                fontName: 'Aldrich',
+              ),
+              SizedBox(height: 36.h),
+              CustomButton(
+                color: AppColors.primaryColor,
+                title: AppString.logIn,
+                titlecolor: Colors.white,
+                onpress: () {},
+              ),
+              SizedBox(height: 16.h),
+              CustomButton(
+                color: Colors.white,
+                titlecolor: AppColors.primaryColor,
+                title: AppString.signUp,
+                onpress: () {},
+              ),
+              SizedBox(height: 66.h),
             ],
           ),
         ),
