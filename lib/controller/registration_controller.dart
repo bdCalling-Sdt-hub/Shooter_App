680c032eb51101485535be329bfc5d8c343aa3cd
mrswapon <mrswapon15@gmail.com>
2024-04-25 17:47:05 +0600

    Matches register completed

--- lib/controller/registration_controller.dart
+++ lib/controller/registration_controller.dart
@@ -8,7 +8,7 @@ import 'package:shooter_app/service/api_constant.dart';
 
 import '../routes/app_routes.dart';
 
-class RegistrationController extends GetxController{
+class RegistrationController extends GetxController {
   final nameController = TextEditingController();
   final emailController = TextEditingController();
   final phoneNumberController = TextEditingController();
@@ -16,24 +16,25 @@ class RegistrationController extends GetxController{
   final clubNameController = TextEditingController();
   String groupValue = '';
 
-
-
   var registerLoading = false.obs;
-  matchRegister(String matchId)async{
+  var bearerToken = ApiClient.bearerToken;
+  matchRegister(String matchId) async {
+    Map<String, String> header = {'Content-Type': 'application/json', 'Authorization': 'Bearer $bearerToken'};
     var age = int.parse(ageController.text);
     var body = {
       "fullName": nameController.text,
-      "email":  emailController.text.trim(),
+      "email": emailController.text.trim(),
       "phone": phoneNumberController.text,
       "gender": groupValue.toString(),
       "age": age.toInt(),
       "clubName": clubNameController.text
     };
-    // Map<String, String> header = {'Content-Type': 'application/json'};
-    var response = await ApiClient.postData(ApiConstant.matchRegister(matchId), json.encode(body));
-    if(response.statusCode == 200 ){
+    var response = await ApiClient.postData(
+        ApiConstant.matchRegister(matchId), json.encode(body),
+        headers: header);
+    if (response.statusCode == 200) {
       print('==================> Register Success');
-      Get.toNamed(AppRoutes.myMatchesScreen,
+      Get.offAndToNamed(AppRoutes.myMatchesScreen,
           parameters: {"matchId": matchId, "screenType": "register"});
     } else {
       Fluttertoast.showToast(msg: response.statusText ?? "");
