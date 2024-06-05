6d6223be49a0319cf318f425467de075df615056
mrswapon <mrswapon15@gmail.com>
2024-06-05 12:08:55 +0600

    some issue fix

--- lib/controller/scores_controller.dart
+++ lib/controller/scores_controller.dart
@@ -6,14 +6,12 @@ import '../service/api_client.dart';
 import '../service/api_constant.dart';
 
 class ScoresController extends GetxController {
-
   @override
   void onInit() {
     // TODO: implement onInit
     super.onInit();
     getMyScores();
     // getScores();
-
   }
 
 //===============================> Get Scores <=================================
@@ -29,25 +27,18 @@ class ScoresController extends GetxController {
     }
   }
 
-
 //===============================> Get Event <=================================
-  RxList eventModel = [].obs;
+  RxList events = [].obs;
   RxBool eventLoading = false.obs;
   getEvent([String matchId = '']) async {
     scoresLoading(true);
     var response = await ApiClient.getData(ApiConstant.myEventList);
     if (response.statusCode == 200) {
-      eventModel.value = response.body['data']['attributes'];
-      print('========================>...$eventModel');
+      events.value = response.body['data']['attributes'];
+      print('========================>...$events');
       eventLoading(false);
-
     }
   }
-
-
-
-
-
 
   ///=====================my scrore=====================>
   RxList myScroesList = [].obs;
@@ -55,6 +46,20 @@ class ScoresController extends GetxController {
   getMyScores() async {
     myScoresLoading(true);
     var response = await ApiClient.getData(ApiConstant.myScoreEndPoint);
+
+    if (response.statusCode == 200) {
+      myScroesList.value = response.body['data']['attributes'];
+      myScoresLoading(false);
+    }
+  }
+
+  ///=====================my scrore=====================>
+  RxList myScoresGraph = [].obs;
+  RxBool myScoresGraphLoading = false.obs;
+  getMyScoresGraph(String eventNmae) async {
+    myScoresLoading(true);
+    var response =
+        await ApiClient.getData(ApiConstant.myScoreEndPointGraph(eventNmae));
 
     if (response.statusCode == 200) {
       myScroesList.value = response.body['data']['attributes'];
--- lib/views/screens/scoresScreen/myScores/inner_widgets/line_chart.dart
+++ lib/views/screens/scoresScreen/myScores/inner_widgets/line_chart.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
+import 'package:shooter_app/controller/my_event_list_controller.dart';
 import 'package:shooter_app/model/events_model.dart';
 import 'package:shooter_app/utils/dimentions.dart';
 import 'package:shooter_app/views/widgets/custom_text.dart';
@@ -37,6 +38,12 @@ class _LineChartState extends State<LineChart> {
   }
 
   @override
+  void initState() {
+    _scoresController.getEvent();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     List<Map<String, dynamic>> convertedChartData = _convertChartData();
 
@@ -45,117 +52,123 @@ class _LineChartState extends State<LineChart> {
       child: ListView(
         physics: const NeverScrollableScrollPhysics(),
         children: [
-          // Dropdown Button Container
-          Padding(
+          // ===================================> Dropdown Button Container <=======================
+         /* Padding(
             padding: const EdgeInsets.all(8.0),
             child: Obx(() {
-              final events = _scoresController.eventModel;
-              return DropdownButtonFormField<EventsModel>(
-                icon: const Icon(Icons.keyboard_arrow_down_sharp, color: Colors.white),
-                value: _selectedItem,
-                onChanged: (value) {
-                  setState(() {
-                    _selectedItem = value;
-                  });
-                },
-                validator: (value) {
-                  if (value == null) {
-                    return "Select Event";
-                  }
-                  return null;
-                },
-                decoration: InputDecoration(
-                  contentPadding: EdgeInsets.symmetric(vertical: 12.h, horizontal: 12.w),
-                  isDense: true,
-                  filled: true,
-                  hintText: 'Select Event',
-                  hintStyle: const TextStyle(color: Color(0xff767676)),
-                ),
-                items: events.map((event) {
-                  return _dropdownMenuItem(event);
-                }).toList(),
-              );
+              return DropdownButtonFormField(
+                  icon: const Icon(Icons.keyboard_arrow_down_sharp,
+                      color: Colors.white),
+                  value: _selectedItem,
+                  onChanged: (value) {
+                    setState(() {
+                      _selectedItem = value;
+                    });
+                  },
+                  validator: (value) {
+                    if (value == null) {
+                      return "Select Event";
+                    }
+                    return null;
+                  },
+                  decoration: InputDecoration(
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 12.h, horizontal: 12.w),
+                    isDense: true,
+                    filled: true,
+                    hintText: 'Select Event',
+                    hintStyle: const TextStyle(color: Color(0xff767676)),
+                  ),
+                  items: List.generate(
+                    _scoresController.events.length,
+                    (index) =>
+                        _dropdownMenuItem(_scoresController.events[index]),
+                  ));
             }),
           ),
-          SizedBox(height: 12.h),
-          // Line Chart Container
+          SizedBox(height: 12.h),*/
+          //=====================================>  Line Chart Container <==================================
           Container(
             width: Get.width.w,
             alignment: Alignment.center,
             child: convertedChartData.length == 1
                 ? const Center(child: CustomText(text: 'No data found!'))
                 : Container(
-              height: 440.h,
-              width: 326 + (10 * widget.chartData.length).toDouble(),
-              padding: EdgeInsets.symmetric(horizontal: 8.w),
-              decoration: BoxDecoration(
-                color: const Color(0xff302d2d),
-                borderRadius: BorderRadius.circular(Dimensions.radiusDefault.r),
-                border: Border.all(color: AppColors.primaryColor),
-              ),
-              child: SingleChildScrollView(
-                scrollDirection: Axis.horizontal,
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.vertical,
-                  child: SizedBox(
-                    width: MediaQuery.of(context).size.width * 1.5,
-                    child: SfCartesianChart(
-                      primaryXAxis: const CategoryAxis(
-                        initialVisibleMinimum: 0.0,
-                        labelPosition: ChartDataLabelPosition.inside,
-                        title: AxisTitle(text: "Match Name"),
-                      ),
-                      primaryYAxis: const NumericAxis(
-                        initialVisibleMinimum: 0.0,
-                        title: AxisTitle(text: "Score"),
-                      ),
-                      legend: const Legend(isVisible: true),
-                      title: const ChartTitle(text: "Scores Ratio"),
-                      tooltipBehavior: TooltipBehavior(enable: true),
-                      series: [
-                        SplineSeries<Map<String, dynamic>, String>(
-                          initialIsVisible: true,
-                          isVisibleInLegend: true,
-                          dataSource: convertedChartData,
-                          xValueMapper: (data, _) => data["matchName"],
-                          yValueMapper: (data, _) => data["score"],
-                          markerSettings: const MarkerSettings(
-                            shape: DataMarkerType.circle,
-                            isVisible: true,
+                    height: 440.h,
+                    width: 326 + (10 * widget.chartData.length).toDouble(),
+                    padding: EdgeInsets.symmetric(horizontal: 8.w),
+                    decoration: BoxDecoration(
+                      color: const Color(0xff302d2d),
+                      borderRadius:
+                          BorderRadius.circular(Dimensions.radiusDefault.r),
+                      border: Border.all(color: AppColors.primaryColor),
+                    ),
+                    child: SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: SingleChildScrollView(
+                        scrollDirection: Axis.vertical,
+                        child: SizedBox(
+                          width: MediaQuery.of(context).size.width * 1.5,
+                          child: SfCartesianChart(
+                            primaryXAxis: const CategoryAxis(
+                              initialVisibleMinimum: 0.0,
+                              labelPosition: ChartDataLabelPosition.inside,
+                              title: AxisTitle(text: "Match Name"),
+                            ),
+                            primaryYAxis: const NumericAxis(
+                              initialVisibleMinimum: 0.0,
+                              title: AxisTitle(text: "Score"),
+                            ),
+                            legend: const Legend(isVisible: true),
+                            title: const ChartTitle(text: "Scores Ratio"),
+                            tooltipBehavior: TooltipBehavior(enable: true),
+                            series: [
+                              SplineSeries<Map<String, dynamic>, String>(
+                                initialIsVisible: true,
+                                isVisibleInLegend: true,
+                                dataSource: convertedChartData,
+                                xValueMapper: (data, _) => data["matchName"],
+                                yValueMapper: (data, _) => data["score"],
+                                markerSettings: const MarkerSettings(
+                                  shape: DataMarkerType.circle,
+                                  isVisible: true,
+                                ),
+                                color: Colors.red,
+                                name: "Match Score",
+                                legendIconType: LegendIconType.circle,
+                                dataLabelSettings: const DataLabelSettings(
+                                    isVisible: true, showZeroValue: true),
+                                enableTooltip: true,
+                                dataLabelMapper: (data, _) {
+                                  final matchDate = data["matchDate"];
+                                  if (matchDate != null &&
+                                      matchDate.isNotEmpty) {
+                                    final dateTime = DateFormat("yyyy-MM-dd")
+                                        .parse(matchDate);
+                                    return DateFormat("MM-dd-yyyy")
+                                        .format(dateTime);
+                                  } else {
+                                    return null;
+                                  }
+                                },
+                              ),
+                            ],
                           ),
-                          color: Colors.red,
-                          name: "Match Score",
-                          legendIconType: LegendIconType.circle,
-                          dataLabelSettings: const DataLabelSettings(
-                              isVisible: true, showZeroValue: true),
-                          enableTooltip: true,
-                          dataLabelMapper: (data, _) {
-                            final matchDate = data["matchDate"];
-                            if (matchDate != null && matchDate.isNotEmpty) {
-                              final dateTime = DateFormat("yyyy-MM-dd").parse(matchDate);
-                              return DateFormat("MM-dd-yyyy").format(dateTime);
-                            } else {
-                              return null;
-                            }
-                          },
                         ),
-                      ],
+                      ),
                     ),
                   ),
-                ),
-              ),
-            ),
           ),
         ],
       ),
     );
   }
 
-  DropdownMenuItem<EventsModel> _dropdownMenuItem(EventsModel value) {
+  DropdownMenuItem _dropdownMenuItem(value) {
     return DropdownMenuItem(
       value: value,
       child: CustomText(
-        text: value.eventName ?? "",
+        text: value['eventName'] ?? "",
       ),
     );
   }
